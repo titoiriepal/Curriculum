57fb2b419539435bf27fd9580de1ab81464a3ac7
DESKTOP-LO3FN3B\TitoI <titoiriepal@gmail.com>
2022-04-24 13:51:09 +0200

    First view of MAIN

--- views/main/index.php
+++ views/main/index.php
@@ -1,7 +1,60 @@
 
 
 <div class="contenedor">
+    <div class="apartado">
+        <h3>Objetivos</h3>
+        <p>
+            Soy una persona activa, con muchas ganas de trabajar en el sector de la programación y diseño de aplicaciones informáticas. 
+            Tengo aptitud para el trabajo en equipo y la resolución de problemas.
+            Estoy dispuesto a formarme desde cualquier nivel para conseguir
+            mis objetivos y los de la empresa
+        </p>
+        <button href="" class="boton">Ver más</button>
+    </div>
 
-    
+    <div class="apartado">
+        <h3>Formación y conocimientos</h3>
+        <p>
+            <span>Técnico superior de desarrollo de aplicaciones informáticas</span><br>
+            I.E.S Brianda de Mendoza 2000-2002<br>
+            <span>IFCT 06-09 Programación de sistemas informáticos</span><br>
+            600 horas con prácticas
+            Centro de estudios Kronos, Guadalajara
+        </p>
+        <button href="" class="boton">Ver más</button>
+    </div>
+
+    <div class="apartado">
+        <h3>Experiencia</h3>
+        <ul>
+            <li>
+                2021-Actualidad Recepcionista <span>GRANDPADEL S.L.</span> 
+            </li>
+            <li>
+                2010 -2021 Autónomo Hostelería
+            </li>
+            <li>
+                2007-2008 <span>Hipercor S.L</span> 
+            </li>
+            <li>
+                2003-2004 Operario impresión <span>D.W.Plastics</span>
+            </li>
+        </ul>
+
+        <button href="" class="boton">Ver más</button>
+    </div>
+
+    <div class="apartado">
+        <h3>Otros datos de interes</h3>
+        <ul>
+            <li>
+                Carnet de conducir
+            </li>
+            <li>
+                Vehículo propio
+            </li>
+        </ul>
+        <button href="" class="boton">Ver más</button>
+    </div>
 </div>
 
