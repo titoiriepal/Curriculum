c9f177cba526c7c3c46f29f24175edb85753dfe7
DESKTOP-LO3FN3B\TitoI <titoiriepal@gmail.com>
2022-05-05 15:30:28 +0200

    meta description added

--- views/layout.php
+++ views/layout.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Curriculum Jesús Ramos, Programador Junior, Programador, desarrollo web, diseño web">
     <title><?php echo $titulo ?? 'Curriculum Web'; ?></title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:wght@300;400;700&display=swap" rel="stylesheet">
